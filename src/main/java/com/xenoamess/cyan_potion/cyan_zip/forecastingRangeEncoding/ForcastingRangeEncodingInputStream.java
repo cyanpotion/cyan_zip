37d9229336e24ca2107786b8ee0124123d0639af
XenoAmess <xenoamess@gmail.com>
2020-08-25 02:00:14 +0800

    add javadocs

--- src/main/java/com/xenoamess/cyan_potion/cyan_zip/forecastingRangeEncoding/ForcastingRangeEncodingInputStream.java
+++ src/main/java/com/xenoamess/cyan_potion/cyan_zip/forecastingRangeEncoding/ForcastingRangeEncodingInputStream.java
@@ -3,20 +3,33 @@ package com.xenoamess.cyan_potion.cyan_zip.forecastingRangeEncoding;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * <p>ForcastingRangeEncodingInputStream class.</p>
+ *
+ * @author XenoAmess
+ * @version 0.1.1
+ */
 public class ForcastingRangeEncodingInputStream extends InputStream {
     protected InputStream inputStream;
     protected ForcastingRangeEncodingDecoder forcastingRangeEncodingDecoder;
 
+    /**
+     * <p>Constructor for ForcastingRangeEncodingInputStream.</p>
+     *
+     * @param inputStream a {@link java.io.InputStream} object.
+     */
     public ForcastingRangeEncodingInputStream(InputStream inputStream) {
         this.inputStream = inputStream;
         forcastingRangeEncodingDecoder = new ForcastingRangeEncodingDecoder(inputStream);
     }
 
+    /** {@inheritDoc} */
     @Override
     public int read() throws IOException {
         return forcastingRangeEncodingDecoder.decodeSingle();
     }
 
+    /** {@inheritDoc} */
     @Override
     public void close() throws IOException {
         this.inputStream.close();
